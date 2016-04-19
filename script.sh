1fd5e61b0596814cd61bfffbac171bda6a60f181
Shan Chowdhury <shan.chowdhury@shan.chowdhury.ansarada.lcl>
2016-04-20 00:53:55 +1000

    running vault as a container

--- script.sh
+++ script.sh
@@ -6,4 +6,8 @@ usermod -aG docker $Username
 mkdir -p Application
 git clone https://github.com/DevAndOps/JavaHelloWorld.git Application
 chown -R $Username Application 
-
+docker run -d cgswong/vault:latest server -dev
+#vaultContainerID=$(docker ps -q)
+#docker exec -it $vaultContainerID bash
+#export VAULT_ADDR='http://127.0.0.1:8200'
+#vault status
